dc15da2c132c683fd98ba394c466fe102d3f6fdd
EndaMcCabe <33401576+EndaMcCabe@users.noreply.github.com>
2022-12-30 22:13:12 +0000

    Add board class with attributes and display board to terminal

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+class Board:
+    """
+    This class will hold all the attributes of the board for the user and the computer
+    """
+
+    def __init__(self, size, ships, name, type):
+        self.size = size
+        self.ships = []
+        self.name = name
+        self.guesses = []
+        self.type = type
+        self.board = [['.' for x in range(5)] for y in range(5)]
+
+    def showBoard(self):
+        for row in self.board:
+            print(" ".join(row))
+
+
+test = Board(5, 4, 'User', 7)
+test.showBoard()
