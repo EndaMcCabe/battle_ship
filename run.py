87c96565180c90e7e802d3ae974ce706efdc442e
EndaMcCabe <33401576+EndaMcCabe@users.noreply.github.com>
2022-12-31 00:45:38 +0000

    Add the guess method to check if the guess from the user is the same as the ship in the ships variable in Board class

--- run.py
+++ run.py
@@ -4,17 +4,28 @@ class Board:
     This class will hold all the attributes of the board for the user and the computer
     """
 
-    def __init__(self, size, ships, name, type):
+    def __init__(self, size, ships, name, user):
         self.size = size
         self.ships = []
         self.name = name
         self.guesses = []
-        self.type = type
+        self.user = user
         self.board = [['.' for x in range(5)] for y in range(5)]
 
     def showBoard(self):
         for row in self.board:
             print(" ".join(row))
+
+    def guess(self):
+        self.guesses.append((x, y))
+        self.board[x][y] = 'X'
+
+        if (x, y) in self.ships:
+            self.board[x][y] = 'x'
+            return 'Hit'
+        else:
+            return "Miss"
+    
 
 
 test = Board(5, 4, 'User', 7)
