233b7809fb64b5bfd5d746c0bcd6c4e47e9ec1bf
EndaMcCabe <33401576+EndaMcCabe@users.noreply.github.com>
2023-01-01 00:02:54 +0000

    Add add ship method so there is the method needed to create the ships for the board

--- run.py
+++ run.py
@@ -4,13 +4,14 @@ class Board:
     This class will hold all the attributes of the board for the user and the computer
     """
 
-    def __init__(self, size, ships, name, user):
+    def __init__(self, size, num, ships, name, user):
         self.size = size
+        self.num = num
         self.ships = []
         self.name = name
         self.guesses = []
         self.user = user
-        self.board = [['.' for x in range(5)] for y in range(5)]
+        self.board = [['.' for x in range(size)] for y in range(size)]
 
     def showBoard(self):
         for row in self.board:
@@ -26,7 +27,14 @@ class Board:
         else:
             return "Miss"
     
+    def add_ship(self, x, y, type="computer"):
+        if len(self.ships) >= self.ships:
+            pass
+        else:
+            self.ships.append((x, y))
+            if self.user == 'User':
+                self.board[x][y] = "O"
 
 
-test = Board(5, 4, 'User', 7)
+test = Board(5, 4, 5, 'User', 7)
 test.showBoard()
